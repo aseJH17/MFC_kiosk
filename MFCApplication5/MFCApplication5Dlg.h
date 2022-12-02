3eb0f738ce36eb52684a8598b6fe52166d87a3f7
Lee jh <asewe77@naver.com>
2022-12-02 18:39:58 +0900

    초기 커밋.

---
+++ MFCApplication5/MFCApplication5Dlg.h
@@ -0,0 +1,61 @@
+﻿
+// MFCApplication5Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplication5Dlg 대화 상자
+class CMFCApplication5Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication5Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION5_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CButton m_Fork;
+	CButton m_Shrimp;
+	CButton m_Chicken;
+	CButton m_Chesse;
+	CButton m_Beaf;
+	CButton m_Squid;
+
+	CButton m_Buger;
+	CButton m_Drink;
+	CButton m_Side;
+	CButton m_Dessert;
+
+	CButton m_Coke;
+	CButton m_Cider;
+	CButton m_Zerocoke;
+	CButton m_Zerocider;
+	CButton m_Milkshake;
+	CButton m_Coffee;
+
+
+
+	afx_msg void OnBnClickedBuger();
+	afx_msg void OnBnClickedDrink();
+	afx_msg void OnBnClickedSide();
+	afx_msg void OnBnClickedDessert();
+	
+};
---
+++ MFCApplication5/resource.h
@@ -0,0 +1,50 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication5.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION5_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDB_CHESSE                      130
+#define IDB_CHICKEN                     131
+#define IDB_BEAF                        132
+#define IDB_FORK                        133
+#define IDB_SHRIMP                      134
+#define IDB_SQUID                       135
+#define IDB_COFFEE                      136
+#define IDB_CIDER                       137
+#define IDB_COKE                        138
+#define IDB_MILKSHAKE                   139
+#define IDB_ORANGE                      140
+#define IDB_TANSAN                      141
+#define IDB_ZEROCIDER                   142
+#define IDB_ZEROCOKE                    143
+#define IDC_BUGER                       1000
+#define IDC_DRINK                       1001
+#define IDC_SIDE                        1002
+#define IDC_DESSERT                     1003
+#define IDC_CHESSE                      1004
+#define IDC_BEAF                        1005
+#define IDC_SQUID                       1006
+#define IDC_FORK                        1007
+#define IDC_SHRIMP                      1008
+#define IDC_CHICKEN                     1009
+#define IDC_COKE                        1010
+#define IDC_CIDER                       1011
+#define IDC_ZEROCOKE                    1012
+#define IDC_ZEROCIDER                   1013
+#define IDC_MILKSHAKE                   1014
+#define IDC_COFFEE                      1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
